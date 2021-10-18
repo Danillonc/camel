806160764c30c4b83a77316021c4e8323428b84e
Danillo Coradello <danillonc@live.com>
2021-10-18 17:07:17 -0300

    integracao com rest api

---
+++ came--quarkus/src/main/java/br/com/camel/quarkus/EbookItemResource.java
@@ -0,0 +1,24 @@
+package br.com.camel.quarkus;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/ebook/item")
+public class EbookItemResource {
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response postEbook(String data) {
+        System.out.println("Entrada: "+ data);
+        return Response.status(200).build();
+    }
+
+    @GET
+    public Response getEbook(@QueryParam("pedidoId") String pedidoId, @QueryParam("ebookId") String ebookId, @QueryParam("clienteId") String clienteId) {
+        System.out.printf("Ebook ID: %s, Cliente ID: %15s, Pedido ID: %s %n", ebookId, clienteId, pedidoId);
+        return Response.status(200).build();
+    }
+}
---
+++ came--quarkus/src/main/java/br/com/camel/quarkus/GreetingConfig.java
@@ -0,0 +1,12 @@
+package br.com.camel.quarkus;
+
+import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithName;
+
+@ConfigMapping(prefix = "greeting")
+public interface GreetingConfig {
+
+    @WithName("message")
+    String message();
+
+}
